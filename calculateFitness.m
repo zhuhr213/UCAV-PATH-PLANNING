85dd07095ff288020a664ed154d8f7f84df722e8
max980213 <49967356+max980213@users.noreply.github.com>
2020-07-13 10:54:17 +0800

    Add files via upload

--- calculateFitness.m
+++ calculateFitness.m
@@ -1,6 +1,7 @@
 function fFitness=calculateFitness(fObjV)
 fFitness=zeros(size(fObjV));
 ind=find(fObjV>=0);
-fFitness(ind)=1./(fObjV(ind)+1);  % 
+fFitness(ind)=1./(fObjV(ind)*0.5+1);   % 设为0.5效果是最好的，对于布谷鸟来说
+% fFitness(ind) = exp(-fObjV(ind)*0.05);
 ind=find(fObjV<0);  % 怎么会有小于0的呢？!
 fFitness(ind)=1+abs(fObjV(ind));
